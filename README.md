cc392d3a2bafe355b0a2c4582663b3d579cd217d
Josh Juran <jjuran@metamage.com>
2014-06-23 18:29:49 -0700

    Add README and check.pl (Initial commit)

---
+++ README.md
@@ -0,0 +1,7 @@
+Project Euler solutions
+=======================
+
+Here are my solutions to the problems at <https://projecteuler.net/>, and a test harness for checking them.
+
+Multiple solutions per problem are allowed.  The test harness accepts multiple arguments, which are each either a test number or a path to a solution (which must be relative and contain the problem number as the first directory component).  Running the test harness with no arguments checks the solutions for all problems to which it has answers.
+
---
+++ check.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+my %answers =
+(
+);
+
+my $n_fails = 0;
+
+sub check_answer
+{
+	my ( $i, $path ) = @_;
+	
+	die "No answer recorded for problem #$i\n" if !exists $answers{ $i };
+	
+	-e $path or die "$path: Doesn't exist\n";
+	-f $path or die "$path: Not a file\n";
+	-x $path or die "$path: Not executable\n";
+	
+	my $answer = $answers{ $i };
+	
+	my $submission = substr( `$path`, 0, -1 );
+	
+	if ( $submission != $answer )
+	{
+		++$n_fails;
+		
+		warn "$path: $submission != $answer\n";
+	}
+}
+
+sub check_problem
+{
+	my ( $i ) = @_;
+	
+	opendir my $d, "$i" or die "$i: $!\n";
+	
+	my @items = grep { m{^ [^.] }x } readdir( $d );
+	
+	warn "No solutions for $i\n" if !@items;
+	
+	foreach my $name ( @items )
+	{
+		check_answer( $i, "$i/$name" );
+	}
+}
+
+sub check
+{
+	foreach ( @_ )
+	{
+		my ($i) = m{^ ( [0-9]+ ) (/ .*)? $}x or die "Bad argument '$_'\n";
+		
+		-f $_ ? check_answer( $i, $_ ) : check_problem( $i );
+	}
+}
+
+my @args = @ARGV ? @ARGV : keys %answers;
+
+check( @args );
+
+exit $n_fails ? 1 : 0;
